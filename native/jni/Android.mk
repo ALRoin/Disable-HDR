227b4661ca1589645fefcae7f9a7ec572c4e7699
Roz <rosihanadhitama@gmail.com>
2026-08-02 17:29:47 +0700

    Update Android.mk

--- native/jni/Android.mk
+++ native/jni/Android.mk
@@ -3,8 +3,5 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := hdr_block
 LOCAL_SRC_FILES := hdr_block.cpp
-LOCAL_STATIC_LIBRARIES := libcxx
-LOCAL_LDLIBS := -llog
+LOCAL_LDLIBS := -llog -lstdc++
 include $(BUILD_SHARED_LIBRARY)
-
-include jni/libcxx/Android.mk
