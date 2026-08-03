c284555beec697e0caea0666947b45936d66667e
Roz <rosihanadhitama@gmail.com>
2026-08-03 12:59:19 +0700

    Update Application.mk for Android NDK settings

--- native/jni/Application.mk
+++ native/jni/Application.mk
@@ -1,4 +1,5 @@
-APP_ABI      := armeabi-v7a arm64-v8a x86 x86_64
-APP_CPPFLAGS := -std=c++17 -fno-exceptions -fno-rtti -fvisibility=hidden -fvisibility-inlines-hidden
-APP_STL      := none
-APP_PLATFORM := android-21
+APP_ABI := arm64-v8a armeabi-v7a x86 x86_64
+APP_PLATFORM := android-26
+APP_STL := c++_static
+APP_CPPFLAGS := -std=c++17 -fvisibility=hidden -fdata-sections -ffunction-sections
+APP_LDFLAGS := -Wl,--gc-sections
