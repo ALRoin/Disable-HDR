a5c5a8a66881f8b617bdf52c9da7386d02859c4c
Roz <rosihanadhitama@gmail.com>
2026-08-03 13:00:37 +0700

    Update Android.mk

--- native/jni/Android.mk
+++ native/jni/Android.mk
@@ -3,5 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := disable_hdr
 LOCAL_SRC_FILES := disable_hdr.cpp
-LOCAL_LDLIBS := -llog -lstdc++
+LOCAL_LDLIBS := -log -landroid
+LOCAL_C_INCLUDES := $(LOCAL_PATH)
+
 include $(BUILD_SHARED_LIBRARY)
