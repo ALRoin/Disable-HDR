17e5d8594137a1d6688b019c1144fc737c6e3f12
Roz <rosihanadhitama@gmail.com>
2026-08-04 18:35:24 +0700

    Delete native/jni directory

--- native/jni/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := disable_hdr
-LOCAL_SRC_FILES := disable_hdr.cpp
-LOCAL_LDLIBS := -log -landroid
-LOCAL_C_INCLUDES := $(LOCAL_PATH)
-
-include $(BUILD_SHARED_LIBRARY)
--- native/jni/Application.mk
+++
@@ -1,5 +0,0 @@
-APP_ABI := arm64-v8a armeabi-v7a x86 x86_64
-APP_PLATFORM := android-26
-APP_STL := c++_static
-APP_CPPFLAGS := -std=c++17 -fvisibility=hidden -fdata-sections -ffunction-sections
-APP_LDFLAGS := -Wl,--gc-sections
